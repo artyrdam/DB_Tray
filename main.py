4bbf0a711010a474ae61c92ab3b20f117e88e3ae
Adam O'Brien <adam.obrien@brightstarr.com>
2021-02-08 11:39:33 +0100

    done

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import csv
-import os
 import openpyxl
 from tkinter import filedialog as fd
 
@@ -14,19 +12,18 @@ def get_excel_data():
     # prints all objects in column 1
     for i in range(1, sheet_obj.max_row + 1):
         cell_obj = sheet_obj.cell(row=i, column=1)
-        #print(cell_obj.value)
+        # print(cell_obj.value)
         data_list.append(cell_obj.value)
+
     # remove dupes
     data_list = list(dict.fromkeys(data_list))
 
     x = path.split(".")[0]
-    test = x + ".csv"
-    #print("path: " + test)
+    new_csv_file = x + ".csv"
+    # print("path: " + new_csv_file)
 
-    with open(test, 'w') as myfile:
-        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-        for val in data_list:
-            wr.writerow([val])
+    with open(new_csv_file, 'w') as myfile:
+        myfile.write("\n".join(data_list))
 
 
 if __name__ == "__main__":
